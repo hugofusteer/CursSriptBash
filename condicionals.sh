b1b25438ddeb84e90da52c306620245834630cba
debian <debian@debian.debian>
2023-04-24 09:13:12 +0200

    Script Condicional

--- condicionals.sh
+++ condicionals.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
-
 read -p "Introdueix X: " X
 
-if [ ${X} -le 5 ]
+if [ ${X} -lt 5 ]
 then 
-    echo "Has introduït un número menor o igual que 5"
+    echo "Has introduït un número menor que 5"
+elif [ ${X} -gt 5 ]
+then
+    echo "Has introduït un número major que 5"
 else
-    echo "Has introduït un número més gran que 5"
+    echo "Has introduït un número igual a 5"
 fi
-
