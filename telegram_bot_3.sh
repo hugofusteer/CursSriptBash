97e414ba96a253d255af3acaf0f8cd147a6e885a
hugo <martinfusterhugo@gmail.com>
2023-05-15 08:41:52 +0200

    Bot

---
+++ telegram_bot_3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IP=$(curl ifconfig.me)
+DATA=$(date)
+HOST=$(hostname)
+MENSAJE="S'ha reiniciat ${HOST} a les ${DATA} ip: ${IP}"
+
+/home/estudiant/GitHub/CursSriptBash/telegram_bot_2.sh "${MENSAJE}"
+# Notificació de missatges del sistema per Telegram usant un bot
