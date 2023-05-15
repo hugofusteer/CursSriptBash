da5a7124ffc0463c7ce9566e7f450da88eb27e54
Your Name <you@example.com>
2023-05-15 09:36:52 +0200

    Bot

--- telegram_bot_3.sh
+++ telegram_bot_3.sh
@@ -5,5 +5,5 @@ DATA=$(date)
 HOST=$(hostname)
 MENSAJE="S'ha reiniciat ${HOST} a les ${DATA} ip: ${IP}"
 
-/home/estudiant/GitHub/CursSriptBash/telegram_bot_2.sh "${MENSAJE}"
+"/home/hugo/Colegio/CursSriptBash/telegram_bot_3.sh "${MENSAJE}"
 # Notificació de missatges del sistema per Telegram usant un bot.
