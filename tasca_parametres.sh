679128817b1e0872aedae061bf84d086a08a8a35
hugo <martinfusterhugo@gmail.com>
2023-05-26 08:24:44 +0200

    Script Parametres

---
+++ tasca_parametres.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Nom del script
+echo "Nom del script: $0"
+################################
+
+# PATH i filename del script
+echo "PATH i filename del script: $(dirname $0)/$(basename $0)"
+####################################################################
+
+# Número de paràmetres
+num_parametres=$#
+echo "Nombre de paràmetres: $num_parametres"
+##############################################
+
+# Comprova si hi ha almenys un paràmetre
+if [ $num_parametres -eq 0 ]; then
+  echo "El primer paràmetre és obligatori i serà USER_NAME."
+  exit 1
+fi
+####################################################################
+
+# Paràmetres
+parametres=("$@")
+echo "Paràmetres: ${parametres[*]}"
+
+####################################################################
+
+# Generació de contrasenyes per a cada usuari
+for user in "${parametres[@]}"; do
+  password=$(openssl rand -base64 12)
+  echo "Usuari: $user - Contrasenya: $password"
+done
